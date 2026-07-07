2a61ce3d52fdd2eca056ccdd78344479af2cdbda
Ahmed Alghali <ahmedalghali@Ahmeds-MacBook-Air.local>
2026-07-08 02:46:50 +0300

    Add custom mlflow tracking module and declare mlflow dependency
     - Add custom ML tracking codes under flint/mlflow/
        - Add mlflow to dependencies in pyproject.toml
        - Add import test in tests/test_feature_selection.py

---
+++ flint/mlflow/__init__.py
@@ -0,0 +1,13 @@
+from .mlflow_log import (
+    log_nvidia_gpu,
+    log_amd_gpu,
+    log_gpu,
+    log_python
+)
+
+__all__ = [
+    "log_nvidia_gpt",
+    "log_amd_gpu",
+    "log_gpu",
+    "log_python"
+]
